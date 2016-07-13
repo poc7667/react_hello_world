f0ee379f84495b0156d711868ec3e8cc16154dea
poc <poc7667@gmail.com>
2016-07-12 20:16:38 -0700

    workable hello world

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-    entry: 'index.jsx',
+    entry: './index.js',
     output: {
-        filename: 'bundle.js'
+        path: './',
+        filename: 'this_bundle.js'
     },
     module: {
-        loaders: [{
+        loaders:[{
             test: /\.jsx?$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel', // 'babel-loader' is also a legal name to reference
+            loader: 'babel-loader',
+            exclude: /node_modules/,
             query: {
-                presets: ['es2015','react']
+                presets: ['es2015', 'react']
             }
         }]
     }
-};
+}
